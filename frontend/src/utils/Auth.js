5e05226ae37f8cfa86f727b55a381514813207d5
JetMod <enver027@yandex.ru>
2024-02-03 09:42:20 +0300

    sprint 15

--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -1,4 +1,4 @@
-//const baseUrl = 'http://localhost:3000'
+//const baseUrl = 'http://localhost:3000';
 const baseUrl = 'https://api.prost.nomoredomainsmonster.ru';
 
 function sendRequestData(url, options) {
