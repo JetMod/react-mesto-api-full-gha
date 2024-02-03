453688ad11a009be51d2367d01e87c66c084fb02
JetMod <enver027@yandex.ru>
2024-02-04 02:12:04 +0300

    sprint 15 9 task

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -3,7 +3,7 @@ import CurrentUserContext from "../contexts/CurrentUserContext";
 
 function Card({ card, onCardClick, onCardLike, onCardDelete }) {
   const currentUser = React.useContext(CurrentUserContext);
-  const isOwner = card.owner._id === currentUser._id;
+  const isOwner = card.owner?._id === currentUser._id;
   const isLiked = card.likes.some((person) => person._id === currentUser._id);
   const activeLikeButtonClassName = "card__like-button_active";
 
